bbfc93e22af39062aa8ec36af533f83242873950
Аля <alla.gorenkova@gmail.com>
2018-05-11 14:57:00 +0300

    Update MainActivity.java

--- java_path/MainActivity.java
+++ java_path/MainActivity.java
@@ -6,7 +6,7 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.Formatter;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,12 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        @SuppressLint("WifiManagerLeak")WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-        TextView details = (TextView) findViewById(R.id.textView);
-        String info = "SSID: " + wifiInfo.getSSID() + "\nStrength: " + wifiInfo.getRssi() + "dBm" + "\nSignal Level: " + WifiManager.calculateSignalLevel(wifiInfo.getRssi(), 5) + "/5" + "\nSpeed: " + wifiInfo.getLinkSpeed() + "Mbps" + "\nIP Address: " + Formatter.formatIpAddress(wifiInfo.getIpAddress()) + "\nMAC Address: " + wifiInfo.getMacAddress() + "\nFrequency: " + "\nHidden SSID: "+ wifiInfo.getHiddenSSID();
-
-        details.setText(info);
     }
 
+    public void check(View view) {
+        @SuppressLint("WifiManagerLeak")WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        String info;
+        TextView details = (TextView) findViewById(R.id.textView);
+        if (wifiManager.isWifiEnabled()) {
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            info = "SSID: " + wifiInfo.getSSID() + "\nStrength: " + wifiInfo.getRssi() + "dBm" + "\nSpeed: " + wifiInfo.getLinkSpeed() + "Mbps";
+        }
+        else {
+            info = "Wi-Fi не подключен";
+        }
+        details.setText(info);
+    }
 }
